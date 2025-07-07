21b20b8b4dc84a28248333ac50f91e60764cfd49
Lenya Nab <nabiev.lenya@gmail.com>
2025-07-07 19:58:16 +0400

    Add comments

--- goroutines/003/003.go
+++ goroutines/003/003.go
@@ -2,18 +2,18 @@ package main
 
 import "fmt"
 
-func generate(readCh chan<- int) {
+func generate(readCh chan<- int) { // канал только для записи
 	for i := 10; i <= 15; i++ {
 		readCh <- i // записываем в канал
 	}
 	close(readCh) // закрываем канал, т.к. записей в него больше не буден
 }
 
-func double(readCh <-chan int, writeCh chan<- int) {
+func double(readCh <-chan int, writeCh chan<- int) { // первый канал только чтение, второй канал для записи
 	for v := range readCh {
-		writeCh <- v * 2
+		writeCh <- v * 2 // увеличиваем на два и записываем в канал
 	}
-	close(writeCh)
+	close(writeCh) // закрываем канал, т.к. записей больше не будет
 }
 
 func main() {
